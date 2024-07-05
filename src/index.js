4e90fbd643f5adee586e79979693cf0e906d2b12
becklang1987 <119975424+becklang1987@users.noreply.github.com>
2024-07-05 18:15:53 +0800

    1st commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from'react-redux';
+import store from './store';
+import {createBrowserRouter,RouterProvider,redirect} from 'react-router-dom'
+import Login from './pages/login/login'
+import PrivateRoute from './components/PrivateRoutes/privateRoutes';
+import Callback from './pages/callback/callback';
+
+
+const browserRouter = createBrowserRouter([
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/callback',
+    element: <Callback />
+  },
+  {
+    path: '/check',
+    element: <PrivateRoute component={App}/>// 受保护的主页
+  },
+]);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
+    <RouterProvider router={browserRouter}>
     <App />
+    </RouterProvider>
+    </Provider>
   </React.StrictMode>
 );
 
---
+++ src/store/config.js
@@ -0,0 +1,5 @@
+const url = {
+    api: 'https://1528-103-232-212-83.ngrok-free.app'
+}
+
+export default url
